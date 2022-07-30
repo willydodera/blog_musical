d844d61dcb7108af3f94d187a1fb58f7803ea92a
willydodera <willydodera@gmail.com>
2022-07-30 00:11:33 -0300

    mejore el avatar y agregue el eliminar user

--- blog_musical/users_app/views.py
+++ blog_musical/users_app/views.py
@@ -13,25 +13,19 @@ from .models import Avatar
 def login_request(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data = request.POST)
-
         if form.is_valid():
             usuario = form.cleaned_data.get('username')
             contraseña = form.cleaned_data.get('password')
             user = authenticate(username=usuario, password=contraseña)
-
             if user is not None:
                 login(request, user)
                 posts = Post.objects.all()
                 return render(request, "blog_app/blog.html", {"form":form, "mensaje":f"Bienvenido {usuario}", "posts":posts})
-
             else:
                 return render(request, "users_app/login.html", {"form":form, "mensaje":"Usuario o contraseña incorrectos"})
-
         else:
             return render(request, "users_app/login.html", {"form":form, "mensaje":"Formulario invalido"})
-
     form = AuthenticationForm()
-
     return render(request, "users_app/login.html", {"form":form})
 
 
@@ -41,20 +35,29 @@ def logout(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserRegisterForm(request.POST, request.FILES)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data["username"]
-            #msj = f"Usuario {username} creado con éxito"
-            #context = {"form": form, "msj":msj}
-            #return render(request, "users_app/register.html", context)
-            return redirect('create_avatar', username=username)
+            user = User.objects.get(username=username)
+            avatar = Avatar(user=user)
+            avatar.save()
+            form = AuthenticationForm()
+            msj = f"Usuario {username} creado con éxito"
+            context = {"form": form, "msj":msj}
+            return render(request, "main_app/home.html", context)
         else:
             return HttpResponse("Formulario invalido")
     else:
         form = UserRegisterForm()
         context = {"form": form}
         return render(request, "users_app/register.html", context)
+
+
+def delete_user(request):
+    user = request.user
+    user.delete()
+    return redirect('logout')
 
 
 def profile(request):
@@ -66,10 +69,10 @@ def profile(request):
     return render(request, "users_app/profile.html", context)
 
 
-def edit_profile(request, id):
-    user = User.objects.get(id=id)
+def edit_profile(request):
+    user = request.user
     if request.method == "POST":
-        form = UserEditForm(request.POST, instance=user)
+        form = UserEditForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             info = form.cleaned_data
             user.username = info["username"]
@@ -79,25 +82,18 @@ def edit_profile(request, id):
             user.password1 = info["password1"]
             user.password2 = info["password2"]
             user.save()
+            old_avatar = Avatar.objects.get(user=user)
+            if old_avatar.img:
+                old_avatar.delete()
+            avatar = Avatar(user=user, img=info["avatar"])
+            avatar.save()
+            img = avatar.img.url
             msj = f"Usuario {user.username} editado con éxito"
-            context = {"msj":msj, "user":user}
+            context = {"msj":msj, "user":user, "img":img}
             return render(request, "users_app/profile.html", context)
-            
     else:
         form = UserEditForm(instance=user)
         context = {"form": form}
         return render(request, "users_app/edit_profile.html", context)
-
-
-def create_avatar(request, username):
-    # Crea avatar predeterminado.
-    user = User.objects.get(username=username)
-    avatar = Avatar(user=user)
-    avatar.save()
-    form = UserRegisterForm()
-    msj = f"Usuario {username} creado con éxito"
-    context = {"form": form, "msj":msj}
-    return render(request, "users_app/register.html", context)
-
 
 
